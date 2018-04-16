361888e5ca9f392197195d89a29fd2c52e211b3c
Instincts <feminadio@gmail.com>
2018-04-16 01:33:34 +0100

    first commit

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+import { AboutPage } from '../about/about';
+import { ConsultPage } from '../consult/consult';
+import { HomePage } from '../home/home';
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab1Root = HomePage;
+  tab2Root = ConsultPage;
+  tab3Root = AboutPage;
+  
+  constructor() {
+
+  }
+}
